fe25100f87daa0f2f72f98d76d1e4f285bd28649
ruben <rubenmmm3@gmail.com>
2017-03-27 03:09:05 +1300

    fill out init.sh
    check if ~/.chipmunk exists, create if not
    check if ~/.chipmunk/index.json exists, create if not

--- init.sh
+++ init.sh
@@ -1,7 +1,26 @@
-#check if directory ~/chipmunk exists, create if not
+#!/bin/bash
 
-#check if ~/chipmunk/index.csv exists, create if not
+#check if directory ~/chipmunk exists, create if not
+echo "chipmunk init script called"
+
+DIRECTORY=~/.chipmunk/		#cant have spaces in dir name as that would need quotes, but quotes prevent expansion of '~' so :)
+if [ ! -d "$DIRECTORY" ] 
+then
+	mkdir "$DIRECTORY"
+	echo "${DIRECTORY} directory created"
+fi
+
+#check if ~/chipmunk/index.json exists, create if not
+
+if [ ! -a "${DIRECTORY}index.json" ]
+then
+	touch "${DIRECTORY}index.json"
+	> "${DIRECTORY}index.json"	
+	echo "{\"entries\":[]}" > "${DIRECTORY}index.json"
+	echo "${DIRECTORY}index.json created"
+fi
 
 #check if ~/chipmunk/prefs file exists, create if not & populate with text (like username and psswd for upload site, api key if necc'y)
+
 
 
