3f8631717f0108012e736c43f36c4e1702c6e6e0
ruben <rubenmmm3@gmail.com>
2017-03-28 21:05:06 +1300

    replace depreciated -a with -e

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@ fi
 
 #check if ~/chipmunk/index.json exists, create if not
 
-if [ ! -a "${DIRECTORY}index.json" ]
+if [ ! -e "${DIRECTORY}index.json" ]
 then
 	touch "${DIRECTORY}index.json"
 	> "${DIRECTORY}index.json"	
@@ -23,29 +23,33 @@ fi
 
 #check if ~/chipmunk/prefs file exists, create if not & populate with text (like username and psswd for upload site, api key if necc'y)
 
-if [ ! "${DIRECTORY}prefs" ]
+if [ ! -e "${DIRECTORY}prefs" ]
 then
 	touch "${DIRECTORY}prefs"
 	>"${DIRECTORY}prefs"
+	echo "prefs file created"
 	
 fi
 
 #ensure ~/.chipmunk/.current_hash exists, then clear it
 
-if [ ! "${DIRECTORY}.current_hash" ]
+if [ ! -e "${DIRECTORY}.current_hash" ]
 then
 	touch "${DIRECTORY}.current_hash"
+	echo ".current_hash file created.."
 fi
 
 >"${DIRECTORY}.current_hash"
-
+echo ".current_hash file cleared"
 
 #ensure ~/.chipmunk/.current_dir exists, then clear it
 
-if [ ! "${DIRECTORY}.current_dir" ] 
+if [ ! -e "${DIRECTORY}.current_dir" ] 
 then
 	touch "${DIRECTORY}.current_dir"
+	echo ".current_dir file created"
 fi
 
 >"${DIRECTORY}.current_dir"
+echo "current_dir file cleared"
 
