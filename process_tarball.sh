6d410dd7319ce71cd8540073099d7edb46b29cc8
ruben <rubenmmm3@gmail.com>
2017-03-29 16:03:17 +1300

    check existence of directory for qr codes

--- process_tarball.sh
+++ process_tarball.sh
@@ -1,6 +1,7 @@
 #ASSUMES:				#these should be from previous script
 #	~/.chipmunk/.current_dir*	contains path to .archive.tar.gz, path ends in /
 #	~/.chipmunk/.current_hash*	contains hash of .archive.tar.gz
+#	W0RKINGDIR/.archive.tar.gz
 
 
 DBPATH=~/.chipmunk/			#chipmunk directory 
@@ -8,7 +9,7 @@ WORKINGDIR=$(<${DBPATH}.current_dir)	#read working directory from file
 HASH=$(<${DBPATH}.current_hash)		#read hash from file
 
 
-#mkdir  $HASH_temp to save part files and qr codes in to 
+#mkdir  $HASH_temp to save part files and in to 
 
 TEMP_DIR=${WORKINGDIR}${HASH}_temp/
 echo "directory tarball will be split into:"
@@ -24,13 +25,26 @@ else
 	echo "check it yourself, and delete it, then re run this script"
 fi
 
+#create directory for qr codes 
 
-#split tarball with name .archive.tar.gz into part files, save in $DIR/part
+QR_DIR=${WORKINGDIR}${HASH}_qr/
+echo "directory qr codes will be saved into:"
+echo $QR_DIR
 
+if [ ! -d "$QR_DIR" ]
+then
+	mkdir "$QR_DIR"
+	echo "$QR_DIR created"
+else
+	echo "somehow directory already exists, delete it then rerun script"
+fi
 
-#split -b 2953 in $HASH.part_ 
+#split tarball with name .archive.tar.gz into part files, save in $TEMP_DIR/part
+#level 40 QR code with low error correction can contain about 2953 bytes or so. Lets split into 2952 or 2920 for a nice round number
+split -b 2952 ${WORKINGDIR}.archive.tar.gz ${TEMP_DIR}${HASH}.part_ 
 
 #turn each file into qr code, save in $DIR/qr
+
 #qrencode 
 #	-o filename
 #	-l L [error correction minimal
