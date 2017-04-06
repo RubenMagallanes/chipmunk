295c998955d583427c2842bc7262eed3a19a8f64
RubenMagallanes <rubenmmm3@gmail.com>
2017-04-06 18:48:43 +1200

    comment todo

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 #check if client secrets file exists in ~/.chipmunk/cient_secrets
 #call upload_video.py passing in required args. read args from file? 
-
+#redirecto script output to file ${HASH}.url so can be read by download .sh
 
 
 
